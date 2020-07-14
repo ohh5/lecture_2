703deafe8b5c5b3047d2dbd7b58c33386aa9debd
Hyung Ju Oh <ohhj0430@gmail.com>
2020-07-14 17:41:36 +0900

    First Commit

---
+++ print_star_param.py
@@ -0,0 +1,9 @@
+def print_star_param(n):
+    # for _ in range(n):
+    #     print('************')
+    print('***************')
+    print('***************')
+    print('***************')
+    print('***************')
+
+print_star_param(4,2)
